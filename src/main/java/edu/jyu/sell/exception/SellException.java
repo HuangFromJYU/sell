7707bf961b6a655c212b7b15da4fff01073cab8f
Jason <1031500191@qq.com>
2017-12-09 10:44:34 +0800

    自定义异常

---
+++ src/main/java/edu/jyu/sell/exception/SellException.java
@@ -0,0 +1,21 @@
+package edu.jyu.sell.exception;
+
+import edu.jyu.sell.enums.ResultEnum;
+
+/**
+ * 自定义异常
+ *
+ * @author Jason
+ * @create 2017-12-09 10:35
+ **/
+public class SellException extends RuntimeException {
+    /**
+     * 异常码
+     */
+    private Integer code;
+
+    public SellException(ResultEnum resultEnum) {
+        super((resultEnum.getMsg()));
+        this.code = resultEnum.getCode();
+    }
+}
