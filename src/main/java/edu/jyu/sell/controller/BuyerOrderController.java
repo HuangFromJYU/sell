f0ca467a1a16c93135df4a05f9cbc990f23fc29f
Jason <1031500191@qq.com>
2017-12-18 20:49:54 +0800

    完成根据openid返回订单列表信息功能

--- src/main/java/edu/jyu/sell/controller/BuyerOrderController.java
+++ src/main/java/edu/jyu/sell/controller/BuyerOrderController.java
@@ -11,11 +11,12 @@ import edu.jyu.sell.util.ResultVOUtil;
 import edu.jyu.sell.vo.ResultVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -42,20 +43,35 @@ public class BuyerOrderController {
      */
     @PostMapping("/create")
     public ResultVO<Map<String, String>> create(@Valid OrderForm orderForm, BindingResult bindingResult) {
-        if(bindingResult.hasErrors()){
-            log.error("【创建订单】 参数不正确，orderForm={}",orderForm);
-            throw new SellException(ResultEnum.PARAM_ERROR,bindingResult.getFieldError().getDefaultMessage());
+        if (bindingResult.hasErrors()) {
+            log.error("【创建订单】 参数不正确，orderForm={}", orderForm);
+            throw new SellException(ResultEnum.PARAM_ERROR, bindingResult.getFieldError().getDefaultMessage());
         }
         OrderDTO orderDTO = OrderForm2OrderDTO.convert(orderForm);
         List<OrderDetail> orderDetailList = orderDTO.getOrderDetailList();
-        if(CollectionUtils.isEmpty(orderDetailList)){
+        if (CollectionUtils.isEmpty(orderDetailList)) {
             log.error("【创建订单】 购物车不能为空");
             throw new SellException(ResultEnum.CART_EMPTY);
         }
         OrderDTO result = orderService.create(orderDTO);
-        Map<String,String> map = new HashMap<>();
-        map.put("orderId",result.getOrderId());
+        Map<String, String> map = new HashMap<>();
+        map.put("orderId", result.getOrderId());
         return ResultVOUtil.success(map);
+    }
 
+    /**
+     * 订单列表
+     */
+    @GetMapping("/list")
+    public ResultVO<List<OrderDTO>> list(@RequestParam("openid") String openid,
+                                         @RequestParam(value = "page",defaultValue = "0") Integer page,
+                                         @RequestParam(value = "size",defaultValue = "10") Integer size) {
+        if(StringUtils.isEmpty(openid)){
+            log.error("【查询订单列表】 openid为空");
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+        PageRequest request = new PageRequest(page,size);
+        Page<OrderDTO> orderDTOPage = orderService.findList(openid, request);
+        return ResultVOUtil.success(orderDTOPage.getContent());
     }
 }
